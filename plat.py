881139f0cd4016aaaa821ad8f27bc09ff4671965
JaunteZhou <jauntezhou@gmail.com>
2018-12-21 17:26:17 +0800

    add and modified some files

--- plat.py
+++ plat.py
@@ -5,12 +5,17 @@
 from flask import Flask,request
 import show_all
 import json
+
+import central_unit
+
 app=Flask(__name__)
 
 @app.route("/nfvcmd")
 def manage():
     req_content = json.dumps(request.form)
     print(request.form)
+
+    central_unit.proc(req_content)
 
 
 if __name__=='__main__':
