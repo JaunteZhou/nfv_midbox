08129679eb26bc0fd080ad1e4d0da2eee5e21577
JaunteZhou <jauntezhou@gmail.com>
2018-12-25 19:03:25 +0800

    Update plat.py
    test flask

--- plat.py
+++ plat.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 #plat.py
 
-from flask import Flask,request
-import flask
+from flask import Flask, request, jsonify
 import json
 
 import central_unit
@@ -12,15 +11,18 @@ app = Flask(__name__)
 
 @app.route("/nfvcmd", methods=['POST'])
 def manage():
-    if request.method == 'POST':
-        req_content = json.loads(request.form)
-        print(request.form)
+    if request.method != 'POST':
+        abort(400)
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    # req_content = json.loads(request.form)
+    print(request.json)
 
-        ret = central_unit.proc(req_content)
-        # TODO：修改返回值参数，返回给北向管理处
+    # ret = central_unit.proc(req_content)
+    # TODO：修改返回值参数，返回给北向管理处
 
-        ret_json = flask.jsonify(ret[1])
-    return ret_json
+    # ret_json = flask.jsonify(ret[1])
+    return jsonify({"res": "0"}), 201
 
 if __name__=='__main__':
     app.run()
