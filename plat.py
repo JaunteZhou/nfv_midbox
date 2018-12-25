c1ceb43a43ed24cf641db6ae92c9a6d3d720ebf4
JaunteZhou <jauntezhou@gmail.com>
2018-12-25 19:17:14 +0800

    Update plat.py
    test flask

--- plat.py
+++ plat.py
@@ -13,16 +13,14 @@ app = Flask(__name__)
 def manage():
     if request.method != 'POST':
         return jsonify({"res": "0"}), 400
-    if not request.json or not 'title' in request.json:
+    if not request.json:
         return jsonify({"res": "0"}), 400
-    # req_content = json.loads(request.form)
-    print(request.json)
 
-    # ret = central_unit.proc(req_content)
+    print(request.json)
+    ret = central_unit.proc(request.json)
     # TODO：修改返回值参数，返回给北向管理处
 
-    # ret_json = flask.jsonify(ret[1])
-    return jsonify({"res": "0"}), 201
+    return jsonify(ret), 201
 
 if __name__=='__main__':
     app.run()
