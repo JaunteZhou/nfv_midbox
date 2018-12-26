45cee33e6371f75a936cf9f5cb9257b70bb2feef
JaunteZhou <jauntezhou@gmail.com>
2018-12-26 20:04:24 +0800

    update *.py about logs

--- log/log_init.py
+++ log/log_init.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-#log.py
+#log_init.py
 
 import logging
 import logging.handlers
 import datetime
-# from southbound import southbound_log
 
-# LOG_FORMAT = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
-# logging.basicConfig(filename='log/nfv_midbox.log', level=logging.ERROR, format=LOG_FORMAT)
-# # logger = logging.getLogger('nfv_midbox')
+from log.log_config import debug_rotating_log_dir, error_log_dir
 
-logger = logging.getLogger('nfv_midbox')
+logger = logging.getLogger('midbox')
 logger.setLevel(logging.DEBUG)
 
-rf_handler = logging.handlers.TimedRotatingFileHandler('log/all.log', when='midnight', interval=1, backupCount=7, atTime=datetime.time(0, 0, 0, 0))
-rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s"))
+# set handler to file for all(debug) logs, and rotated at midnight
+# 设置针对所有(DEBUG级)日志信息的写入日志文件的handler，并且在每天午夜进行分割
+drf_handler = logging.handlers.TimedRotatingFileHandler(debug_rotating_log_dir, when='midnight', interval=1, backupCount=7, atTime=datetime.time(0, 0, 0, 0))
+drf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - [%(name)s] - %(message)s"))
 
-f_handler = logging.FileHandler('log/error.log')
-f_handler.setLevel(logging.ERROR)
-f_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+# set handler to file for error logs
+# 设置针对ERROR级别及以上的日志信息的写入日志文件的handler
+ef_handler = logging.FileHandler(error_log_dir)
+ef_handler.setLevel(logging.ERROR)
+ef_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - [%(name)s:%(lineno)d] - %(message)s"))
 
-logger.addHandler(rf_handler)
-logger.addHandler(f_handler)
+# add handlers to logger
+logger.addHandler(drf_handler)
+logger.addHandler(ef_handler)
 
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warning message')
-logger.error('error message')
-logger.critical('critical message')
-
-# southbound_log.logger_add_handler([f_handler, rf_handler])
-# southbound_log.test_log()
+# write init logs
+logger.debug('debug init')
+logger.info('info init')
+logger.warning('warning init')
+logger.error('error init')
+logger.critical('critical init')
--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 #main.py
 
-import log_init
-# import plat
-from southbound import southbound_log
+from log import log_init
+from midbox import plat
 
+from midbox.southbound import southbound_log
 southbound_log.test_log()
 
-# plat.app.run()
+plat.app.run()
