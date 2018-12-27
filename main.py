db3593565434b56348e92a193a967d068faba073
JaunteZhou <jauntezhou@gmail.com>
2018-12-27 20:43:01 +0800

    Revert "Update main.py"
    This reverts commit d0798fc27ab96b4c27d9e3b7155f5912036306f2.

--- main.py
+++ main.py
@@ -7,6 +7,11 @@
 import log_init
 log_init.log_init()
 
+# just for test
+# TODO:只是测试使用，后续删除
+# from midbox import _test_log
+# _test_log.test_log()
+
 # run main flask app
 # 运行主程序flask的app
 from midbox import plat
