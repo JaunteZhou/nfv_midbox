b5b6256c2ea33a0bb563b48ed6b1d3341650062b
JaunteZhou <jauntezhou@gmail.com>
2019-03-11 22:13:01 +0800

    modified flows.py

--- midbox/southbound/docker/remote_deploy.py
+++ midbox/southbound/docker/remote_deploy.py
@@ -22,31 +22,63 @@ def container_deploy(ip,password,cpu,mem,image_name,containerid,cip='192.168.1.1
 
     logger=logging.getLogger(__name__)
 
-    args='docker run -d -m '+mem+'M -v /home/dockertest/:/data --cap-add=NET_ADMIN --cpu-period=1000000 --cpu-quota='+cpu+' --net=none --name c'+containerid+' '+image_name+' '
+    args = 'docker run -d -m '+mem+'M -v /home/dockertest/:/data '+\
+            '--cap-add=NET_ADMIN --cpu-period=1000000 --cpu-quota='+cpu+\
+            ' --net=none --name c'+containerid+' '+image_name+' '
     exitstatus,rdata=remote_ssh(ip,password,args)
     logger.info(rdata);
-    exitstatus,rdata=remote_ssh(ip,password,'pid=$(docker inspect -f \'{{.State.Pid}}\' c'+containerid+') && mkdir -p /var/run/netns && ln -s /proc/$pid/ns/net /var/run/netns/$pid && echo $pid')
+    exitstatus,rdata=remote_ssh(ip,password,
+            'pid=$(docker inspect -f \'{{.State.Pid}}\' c'+containerid+') '+\
+            '&& mkdir -p /var/run/netns '+\
+            '&& ln -s /proc/$pid/ns/net /var/run/netns/$pid '+\
+            '&& echo $pid')
     logger.info(rdata);
     pid=rdata;
     pid=re.findall('\d+',pid)[0]
     print('PID:'+pid)
 
-    exitstatus,rdata=remote_ssh(ip,password,'ovs-vsctl add-br sw1 && ovs-vsctl add-br sw-man')
-    exitstatus,rdata=remote_ssh(ip,password,'ip link add in type veth peer name br-c'+containerid+'-in && ip link add out type veth peer name br-c'+containerid+'-out')
+    exitstatus,rdata=remote_ssh(ip,password,
+            'ovs-vsctl add-br sw1 && ovs-vsctl add-br sw-man')
+    exitstatus,rdata=remote_ssh(ip,password,
+            'ip link add in type veth peer name br-c'+containerid+'-in '+\
+            '&& ip link add out type veth peer name br-c'+containerid+'-out')
     logger.info(rdata);
-    exitstatus,rdata=remote_ssh(ip,password,'ip link set in netns '+pid+' && ip link set out netns '+pid+' && ip link set br-c'+containerid+'-in up && ip link set br-c'+containerid+'-out up')
+
+    exitstatus,rdata=remote_ssh(ip,password,
+            'ip link set in netns '+pid+' '+\
+            '&& ip link set out netns '+pid+' '+\
+            '&& ip link set br-c'+containerid+'-in up '+\
+            '&& ip link set br-c'+containerid+'-out up')
     logger.info(rdata);
-    exitstatus,rdata=remote_ssh(ip,password,'ip netns exec '+pid+' ip link set in up && ip netns exec '+pid+' ip link set out up')
+
+    exitstatus,rdata=remote_ssh(ip,password,
+            'ip netns exec '+pid+' ip link set in up '+\
+            '&& ip netns exec '+pid+' ip link set out up')
     logger.info(rdata);
-    exitstatus,rdata=remote_ssh(ip,password,'docker exec c'+containerid+' ovs-vsctl add-br sw && docker exec c'+containerid+' ovs-vsctl add-port sw in &&docker exec c'+containerid+' ovs-vsctl add-port sw out')#容器内网络配置
+
+    #容器内网络配置
+    exitstatus,rdata=remote_ssh(ip,password,
+            'docker exec c'+containerid+' ovs-vsctl add-br sw '+\
+            '&& docker exec c'+containerid+' ovs-vsctl add-port sw in '+\
+            '&& docker exec c'+containerid+' ovs-vsctl add-port sw out')
     logger.info(rdata);
 
     #注意：容器镜像内必须安装OVS2.9以上版本！！
-    exitstatus,rdata=remote_ssh(ip,password,' ifconfig br-c'+containerid+'-in down && ovs-vsctl add-port sw1 br-c'+containerid+'-in && ovs-vsctl add-port sw1 br-c'+containerid+'-out')#断掉回路，等容器启用时再up该接口
+    #断掉回路，等容器启用时再up该接口
+    exitstatus,rdata=remote_ssh(ip,password,
+            'ifconfig br-c'+containerid+'-in down '+\
+            '&& ovs-vsctl add-port sw1 br-c'+containerid+'-in '+\
+            '&& ovs-vsctl add-port sw1 br-c'+containerid+'-out')
     logger.info(rdata);
-    exitstatus,rdata=remote_ssh(ip,password,'ip link add ceth0 type veth peer name br-c'+containerid+' && ip link set ceth0 netns '+pid+' && ip link set br-c'+containerid+' up')
+    exitstatus,rdata=remote_ssh(ip,password,
+            'ip link add ceth0 type veth peer name br-c'+containerid+' '+\
+            '&& ip link set ceth0 netns '+pid+' '+\
+            '&& ip link set br-c'+containerid+' up')
     logger.info(rdata);
-    exitstatus,rdata=remote_ssh(ip,password,'ovs-vsctl add-port sw-man br-c'+containerid+' && ip netns exec '+pid+' ip link set ceth0 up && docker exec c'+containerid+' ifconfig ceth0 '+cip+' up')
+    exitstatus,rdata=remote_ssh(ip,password,
+            'ovs-vsctl add-port sw-man br-c'+containerid+' '+\
+            '&& ip netns exec '+pid+' ip link set ceth0 up '+\
+            '&& docker exec c'+containerid+' ifconfig ceth0 '+cip+' up')
     logger.info(rdata);
     return 0
 
