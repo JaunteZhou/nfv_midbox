c2473f9c53f2748f9831da0c28136bc375c78620
JaunteZhou <jauntezhou@gmail.com>
2019-03-13 22:29:06 +0800

    update auth_token

--- midbox/southbound/openstack/openstack_rest_api/identity.py
+++ midbox/southbound/openstack/openstack_rest_api/identity.py
@@ -9,51 +9,56 @@ logger = logging.getLogger(__name__)
 
 from midbox._config import user_id, password, tenant_id, auth_token_url
 
-def composeAuthPara(user_id, password, proj_id):
-    para = {
-        "auth": {
-            "scope": {
-                "project": {
-                    "id": proj_id
-                }
-            },
-            "identity": {
-                "password": {
-                    "user": {
-                        "password": password,
-                        "id": user_id
+THRESHOLD_TIME_FOR_UPDATE = 600
+
+class AuthToken:
+    def __init__(self):
+        self.auth_token = ""
+        self.expires_time = datetime.datetime.now()
+
+    def composeAuthPara(self, user_id, password, proj_id):
+        para = {
+            "auth": {
+                "scope": {
+                    "project": {
+                        "id": proj_id
                     }
                 },
-                "methods": [
-                    "password"
-                ]
+                "identity": {
+                    "password": {
+                        "user": {
+                            "password": password,
+                            "id": user_id
+                        }
+                    },
+                    "methods": [
+                        "password"
+                    ]
+                }
             }
         }
-    }
-    return json.dumps(para)
+        return json.dumps(para)
 
-def getToken():
-    logger.debug('Start.')
-    para_json = composeAuthPara(user_id, password, tenant_id)
-    headers = {
-        "Content-type": "application/json",
-        "Accept": "application/json"
-    }
-    r = requests.post(auth_token_url, para_json, headers=headers)
-    if r.status_code != requests.codes.created:
-        logger.error((r.status_code, r.json()))
-        return ""
-    return r.headers.get('X-Subject-Token'), r.json()["token"]["expires_at"]
+    def getToken(self):
+        if (self.expires_time - datetime.datetime.now()).total_seconds() < THRESHOLD_TIME_FOR_UPDATE:
+            logger.debug('Start Get Token.')
+            para_json = composeAuthPara(user_id, password, tenant_id)
+            headers = {
+                "Content-type": "application/json",
+                "Accept": "application/json"
+            }
+            r = requests.post(auth_token_url, para_json, headers=headers)
+            if r.status_code != requests.codes.created:
+                logger.error((r.status_code, r.json()))
+                return ""
+            # expires_at = '2019-03-13T14:59:49.000000Z'
+            expires_at = r.json()["token"]["expires_at"]
+            expires_at_for_dt = expires_at.replace("T", " ").split(".")[0]
+            self.expires_time = datetime.datetime.strptime(expires_at_for_dt, "%Y-%m-%d %H:%M:%S")
+            self.auth_token = r.headers.get('X-Subject-Token')
+        
+        return self.auth_token
 
-auth_token, expires_at = getToken()
-# expires_at = '2019-03-13T14:59:49.000000Z'
-expires_str = expires_at.replace("T", " ").split(".")[0]
-logger.debug(expires_str)
-expires_dt = datetime.datetime.strptime(expires_str, "%Y-%m-%d %H:%M:%S")
-
-logger.debug(auth_token)
-logger.debug(datetime.datetime.now())
-logger.debug( (datetime.datetime.now()-expires_dt).total_seconds() )
 
 if __name__ == '__main__':
     print (auth_token)
--- midbox/southbound/openstack/openstack_rest_api/rest_requests.py
+++ midbox/southbound/openstack/openstack_rest_api/rest_requests.py
@@ -4,26 +4,32 @@
 import json
 import requests
 
-from midbox.southbound.openstack.openstack_rest_api.identity import auth_token
+from midbox.southbound.openstack.openstack_rest_api.identity import AuthToken
+
+at = AuthToken()
 
 headers = {
     "Content-type": "application/json",
     "Accept": "application/json",
-    "X-Auth-Token": auth_token
+    "X-Auth-Token": at.getToken()
 }
 
 def get(url, payload=None):
+    headers["X-Auth-Token"] = at.getToken()
     r = requests.get(url, headers=headers, params=payload)
     return r.status_code, r.json()
 
 def post(url, body):
+    headers["X-Auth-Token"] = at.getToken()
     r = requests.post(url, body, headers=headers)
     return r.status_code, r.json()
 
 def put(url, body):
+    headers["X-Auth-Token"] = at.getToken()
     r = requests.put(url, body, headers=headers)
     return r.status_code, r.json()
 
 def delete(url):
+    headers["X-Auth-Token"] = at.getToken()
     r = requests.delete(url, headers=headers)
     return r.status_code, "End"
